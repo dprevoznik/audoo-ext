d5f46609911bf78a3f5080191a88eedb3df3f51f
Daniel <dprevoznik@gmail.com>
2020-06-14 22:20:55 -0400

    Even more initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 
 import * as React from 'react';
 import { hot } from "react-hot-loader/root";
+
 interface Props {
-   name: string
+   main: string,
+   names: string[]
 }
 
-class App extends React.Component<Props> {
-  render() {
-    const { name } = this.props;
-    return <div>Hello {name}</div>;
-  }
-}
+function App(props: Props) {
+  var {main, names} = props;
+  return (
+  <div>Hello {main} {names[1]}</div>
+  )
+};
 
 export default hot(App);
